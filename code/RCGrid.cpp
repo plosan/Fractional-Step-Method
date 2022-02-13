3603ddc80b214bba79f3ab1e534f7169800d0942
plosan <lopezsancha.p@gmail.com>
2022-02-13 21:33:12 +0100

    initial commit

---
+++ code/RCGrid.cpp
@@ -0,0 +1,17 @@
+
+class RCGrid {
+
+private:
+    int nX;         // Nodes in X dir
+    int nY;         // Nodes in Y dir
+    double* nodeX;
+    double* nodeY;
+    double* faceX;
+    double* faceY;
+    double* surfX;
+    double* surfY;
+
+
+public:
+
+};
